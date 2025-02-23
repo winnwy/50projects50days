9017fe17b503c6edc9eb5ac7b1962a5b440ca40a
Winn Z <winnwyz@gmail.com>
2025-02-23 22:07:54 +1100

    Code format

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -2,16 +2,16 @@ const btn_off = document.getElementById("menu-off");
 const btn_on = document.getElementById("menu-on");
 const main_container = document.querySelector(".main-container");
 const navbar = document.querySelector(".navbar");
-const circle = document.querySelector(".circle")
+const circle = document.querySelector(".circle");
 
 btn_off.addEventListener("click", () => {
-    navbar.classList.add("show-navbar")
-    circle.classList.add("show-navbar")
-    main_container.classList.add("show-navbar")
-})
+  navbar.classList.add("show-navbar");
+  circle.classList.add("show-navbar");
+  main_container.classList.add("show-navbar");
+});
 
-btn_on.addEventListener("click", ()=> {
-    navbar.classList.remove("show-navbar")
-    circle.classList.remove("show-navbar")
-    main_container.classList.remove("show-navbar")
-})
+btn_on.addEventListener("click", () => {
+  navbar.classList.remove("show-navbar");
+  circle.classList.remove("show-navbar");
+  main_container.classList.remove("show-navbar");
+});
