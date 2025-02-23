14b501f8090aa911332a922d2276c49496d43cad
Winn Z <winnwyz@gmail.com>
2025-02-23 22:07:23 +1100

    Add button rotate

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -1,19 +1,17 @@
 const btn_off = document.getElementById("menu-off");
 const btn_on = document.getElementById("menu-on");
 const main_container = document.querySelector(".main-container");
-const nav_container = document.querySelector(".nav-container");
 const navbar = document.querySelector(".navbar");
+const circle = document.querySelector(".circle")
 
 btn_off.addEventListener("click", () => {
-    btn_on.style.display = "block";
-    btn_off.style.display = "none";
     navbar.classList.add("show-navbar")
+    circle.classList.add("show-navbar")
     main_container.classList.add("show-navbar")
 })
 
 btn_on.addEventListener("click", ()=> {
-    btn_off.style.display = "block";
-    btn_on.style.display="none";
     navbar.classList.remove("show-navbar")
+    circle.classList.remove("show-navbar")
     main_container.classList.remove("show-navbar")
 })
